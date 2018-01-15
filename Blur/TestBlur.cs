fae97bd837f8ff526a4aa45507faac97e55ff744
聂述龙 <627972381@qq.com>
2018-01-15 23:01:26 +0800

    add Blur

---
+++ Blur/TestBlur.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBlur : MonoBehaviour 
+{
+	private Blur blur;
+
+	void Start () 
+	{
+		blur = gameObject.AddComponent<Blur> ();
+	}
+
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)  
+	{  
+		blur.BlurImage (source, destination);
+	}  
+}
---
+++ RenderAfterPostEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.Rendering;  
+
+public class RenderAfterPostEffect : MonoBehaviour  
+{  
+	private CommandBuffer commandBuffer = null;  
+	private Renderer targetRenderer = null;  
+
+	public Material replaceMaterial = null;  
+
+	void OnEnable()  
+	{  
+		targetRenderer = this.GetComponentInChildren<Renderer>();  
+		if (targetRenderer)  
+		{  
+			commandBuffer = new CommandBuffer();  
+			commandBuffer.DrawRenderer(targetRenderer, replaceMaterial); 
+			Camera.main.AddCommandBuffer(CameraEvent.AfterImageEffects, commandBuffer);  
+			targetRenderer.enabled = false;  
+		}  
+	}  
+
+	void OnDisable()  
+	{  
+		if (targetRenderer)  
+		{  
+			Camera.main.RemoveCommandBuffer(CameraEvent.AfterImageEffects, commandBuffer);  
+			commandBuffer.Clear();  
+			targetRenderer.enabled = true;  
+		}  
+	}  
+}  
